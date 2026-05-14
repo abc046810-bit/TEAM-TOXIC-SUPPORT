06d2f99a80053206cf31efe9a18e8a3a1cdca1ac
abc046810-bit <abc046810@gmail.com>
2026-05-14 14:05:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,5 +211,9 @@ def main():
     print("Bot Running...")
     app.run_polling()
 
+import asyncio
+
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     main()
